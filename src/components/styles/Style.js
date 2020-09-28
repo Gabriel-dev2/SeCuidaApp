def12873b50717d199298188019c7bd5c5163bcc
Gabriel <gabriel.lima@neurotech.com.br>
2020-09-28 20:19:11 -0300

    APP-02 Componente de estilo

---
+++ src/components/styles/Style.js
@@ -0,0 +1,40 @@
+import { StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor: '#B22222'
+  },
+  logo: {
+    width:150,
+    height: 150,
+    borderRadius: 100
+  },
+  input: {
+    marginTop: 10,
+    padding: 10,
+    width: 300,
+    backgroundColor: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+    borderRadius: 3
+  },
+  button: {
+    width: 300,
+    height: 42,
+    backgroundColor: '#4682B4',
+    marginTop: 10,
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+   fontSize: 16,
+   fontWeight: 'bold',
+   color: '#fff'
+  }
+})
+
+export default styles;
